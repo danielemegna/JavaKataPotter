3a0ce1c9c84946c9697a495e61ab7c73513c1b2f
Daniele Megna <megna.dany@gmail.com>
2015-05-12 16:44:52 +0200

    Passing three-different-books test case with shortest path

--- src/KataPotterTest.java
+++ src/KataPotterTest.java
@@ -60,6 +60,15 @@ public class KataPotterTest {
     }
 
     @Test
+    public void three_different_titles__has_ten_percent_discount() {
+        assertBasketCost(((8*3) * 0.90),
+            "Philosophers Stone",
+            "Prisoner of Azkaban",
+            "Order of the Phoenix"
+        );
+    }
+
+    @Test
     @Ignore
     public void final_acceptance_test() {
         assertBasketCost(51.20,
@@ -75,7 +84,7 @@ public class KataPotterTest {
 
         int titlesVariety = new HashSet<>(Arrays.asList(titles)).size();
         if(titlesVariety > 1 && titlesVariety == titles.length)
-            return ((8*2) * 0.95);
+            return ((8*titles.length) * (1 - (0.05 * (titles.length-1))));
 
         return titles.length * 8;
 
