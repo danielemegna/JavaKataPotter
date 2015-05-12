e6ea363d9d5e5cd0d6b6a8ed4e6397841c36c3a3
Daniele Megna <megna.dany@gmail.com>
2015-05-12 15:28:24 +0200

    Added single book basket testcase

--- src/KataPotterTest.java
+++ src/KataPotterTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static junit.framework.Assert.*;
+import static junit.framework.TestCase.*;
 
 /**
  * Created by daniele on 12/05/15.
@@ -12,7 +12,17 @@ public class KataPotterTest {
         assertEquals(0.0, basketCost(new String[]{}));
     }
 
+    @Test
+    public void basket_with_a_books__cost_eight() {
+        assertEquals(8.0, basketCost(new String[]{ "Philosophers Stone" }));
+        assertEquals(8.0, basketCost(new String[]{ "Chamber of Secrets" }));
+        assertEquals(8.0, basketCost(new String[]{ "Prisoner of Azkaban" }));
+    }
+
     private double basketCost(String[] titles) {
+        if(titles.length > 0)
+            return 8;
+
         return 0;
     }
 }
