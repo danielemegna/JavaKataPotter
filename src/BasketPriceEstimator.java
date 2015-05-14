3eb3790aa292b89892021a3c4109054f2fd3f838
Daniele Megna <megna.dany@gmail.com>
2015-05-14 11:07:01 +0200

    Little refactoring

--- src/BasketPriceEstimator.java
+++ src/BasketPriceEstimator.java
@@ -5,19 +5,22 @@ import java.util.HashMap;
  */
 public class BasketPriceEstimator {
 
-    private final HashMap<Integer, Double> discountsMap = new HashMap<Integer, Double>() {{
-        put(1,0.0);
-        put(2,0.05);
-        put(3,0.10);
-        put(4,0.20);
-        put(5,0.25);
-    }};
+    private final double BOOKS_PRICE = 8;
+
+    private final HashMap<Integer, Double> discountsMap =
+        new HashMap<Integer, Double>() {{
+            put(1, 0.0);
+            put(2, 0.05);
+            put(3, 0.10);
+            put(4, 0.20);
+            put(5, 0.25);
+        }};
 
     public double estimate(BookBasket basket) {
         if(basket.getBookCount() == 0)
             return 0;
 
-        double basePrice = (8 * basket.getBookCount());
+        double basePrice = (this.BOOKS_PRICE * basket.getBookCount());
         if(basket.getTitlesVariety() != basket.getBookCount())
             return basePrice;
 
